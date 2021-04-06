c65cb18636832c7214de9a1d67ab5cd20ee828c9
Abrivard <dwain93290@hotmail.fr>
2021-04-06 07:56:07 +0200

    ajout du formulaire de la page panier

--- Frontend/panier.js
+++ Frontend/panier.js
@@ -17,9 +17,53 @@ panierVide.innerHTML= '<p class="text-center">Le panier est vide !</p>'
 
         ligne.innerHTML = //ces balises contiendront : 
         `
-        <td>${element.name}</td><td>${element.color}</td><td><input type="number" id="quantity" min="1" value="${element.quantity}"></td><td id="prix">${element.price}</td>
+        <td>${element.name}</td><td>${element.color}</td><td><input type="number" id="quantity" min="1" value="${element.quantity}"></td><td class="prix">${element.price}</td><button class="sup">Supprimer</button>
         `
         table.appendChild(ligne) // ajout de nos balise 'tr' à notre élément 'table'
 
+
 });
 }
+
+// formulaire avant passage de commande
+
+let form = document.getElementById('confirmation')
+
+form.addEventListener('submit', function(e){
+    e.preventDefault()
+    let erreur = ''
+    let lastName = document.getElementById('lastName')
+    let firstName = document.getElementById('firstName')
+    let email = document.getElementById('email')
+    let email2 = document.getElementById('email2')
+    let mdp = document.getElementById('mdp')
+   
+    
+   if (!lastName.value){
+        erreur = "veuillez renseignez votre nom"
+    }
+    if (!firstName.value){
+    erreur = "veuillez renseignez votre prénom"
+    }
+    if (!email.value){
+        erreur = "veuillez renseignez un e-mail"
+    }
+    if (!email2.value){
+        erreur = "veuillez confirmer votre e-mail"
+    }
+    if(email.value!=email2.value){
+        erreur = "veuillez renseignez deux adresse e-mail identique"
+    }
+    if (!mdp.value){
+        erreur = "veuillez renseignez un mot de passe"
+    }
+    
+    if (erreur){
+       document.getElementById('erreur').innerHTML = erreur
+        return false
+    }else {
+       
+        alert('formulaire envoyé')
+    }
+
+})
