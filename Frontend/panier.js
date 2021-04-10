a47de061649dbe233fa1a5cc83b44be4d4ecded4
Abrivard <dwain93290@hotmail.fr>
2021-04-10 15:37:32 +0200

    ajout de class(page produit) de bouton (page panier) de commentaire et calcul du prix total

--- Frontend/panier.js
+++ Frontend/panier.js
@@ -1,49 +1,94 @@
+// ---------------------------Déclaration de variables -----------------------------------------
+
 //Déclaration de la variable qui contient les éléments du localStorage
 let productLocalStorage = JSON.parse(localStorage.getItem('produit'));
 
-//const produitPanier = document.getElementById('produit-panier')
+//Récupération de la balise qui contiendra un message lorsque le panier sera vide
 const panierVide = document.getElementById('panier-vide')
+
+// Déclaration du tableau qui contiendra les prix de tous les articles du panier
+const prixCalculer = []
+
+//--------------------------Tableau de produit séléctionné-------------------------------
 
 // si mon local Storage est vide 
 if(productLocalStorage === null){
 panierVide.innerHTML= '<p class="text-center">Le panier est vide !</p>'
-//si non
+//sinon
 }else{
 
-    const table = document.getElementById("cart-tablebody") //récupétation de l'élément qui contiendra nos article
+    const table = document.getElementById("cart-tablebody") //récupétation de l'élément qui contiendra nos articles
 
-    productLocalStorage.forEach(element => { // pour chaque element de mon local Storage
-        let ligne  = document.createElement('tr') //Creation d'une balise 'tr'
+    productLocalStorage.forEach(element => { // pour chaque éléments de mon local Storage
+        let ligne  = document.createElement('tr') //Création d'une balise 'tr'
 
         ligne.innerHTML = //ces balises contiendront : 
         `
-        <td>${element.name}</td><td>${element.color}</td><td><input type="number" id="quantity" min="1" value="${element.quantity}"></td><td class="prix">${element.price}</td><button class="sup">Supprimer</button>
+        <td class="productName">${element.name}</td>
+        <td>${element.color}</td>
+        <td>${element.quantity}</td>
+        <td class="prix">${element.price * element.quantity}</td>  
+        <button id="sup" onclick="deleteProductEventHandler('${element.name}')">Supprimer</button>
         `
-        table.appendChild(ligne) // ajout de nos balise 'tr' à notre élément 'table'
+        table.appendChild(ligne) // ajout de nos balises 'tr' à notre élément 'table'
 
-
+        prixCalculer.push(element.price * element.quantity) //Envoie les prix de nos articles au tableau "prixCalculer"
+    
 });
 }
 
-// formulaire avant passage de commande
+//-----------------------------Calcul du prix total------------------------------------------------
 
-let form = document.getElementById('confirmation')
+ // Déclaration d'une variable qui contiendra le calcul de tous les nombres du tableau "prixCalculer"
+const prixTotal = prixCalculer.reduce((x , y) => x + y)
 
-form.addEventListener('submit', function(e){
-    e.preventDefault()
-    let erreur = ''
+let prixFinal = document.getElementById('prixFinal') //récupération de la balise qui affichera le prix
+
+prixFinal.innerHTML= prixTotal 
+
+//------------------------------Supprimer un article du panier--------------------------------------
+
+// création de la fonction qui permettra de supprimer un article
+
+function deleteProductEventHandler(productName){
+    productLocalStorage.forEach((item, index) => {
+        if(item.name == productName){
+            productLocalStorage.splice(index, 1)
+            location.reload()
+            localStorage.setItem("produit", JSON.stringify(productLocalStorage))
+        }
+    })
+}
+
+//--------------------------------Formulaire 'informations personnelles'-----------------------------
+
+let form = document.getElementById('confirmation') //récupération de la balise "form"
+
+form.addEventListener('submit', function(e){ //lors du clic sur le bouton du formulaire
+    e.preventDefault()  //Ne recharge pas la page
+    // récupère les éléments ci-dessous
+    let erreur = '' // cette variable nous permettra d'écrire notre message d'erreur
     let lastName = document.getElementById('lastName')
     let firstName = document.getElementById('firstName')
+    let address = document.getElementById('address')
+    let city = document.getElementById('city')
     let email = document.getElementById('email')
     let email2 = document.getElementById('email2')
-    let mdp = document.getElementById('mdp')
-   
+    let infoCommande = []
     
+   
+    // lorsqu'un élément n'est pas rempli, affiche un message d'erreur dans la variable "erreur"
    if (!lastName.value){
         erreur = "veuillez renseignez votre nom"
     }
     if (!firstName.value){
     erreur = "veuillez renseignez votre prénom"
+    }
+    if (!address.value){
+        erreur = "veuillez renseignez votre adresse"
+    }
+    if(!city.value){
+        erreur = "veuillez renseignez votre ville"
     }
     if (!email.value){
         erreur = "veuillez renseignez un e-mail"
@@ -54,16 +99,24 @@ form.addEventListener('submit', function(e){
     if(email.value!=email2.value){
         erreur = "veuillez renseignez deux adresse e-mail identique"
     }
-    if (!mdp.value){
-        erreur = "veuillez renseignez un mot de passe"
-    }
-    
-    if (erreur){
+    if (erreur){ //Donc si "erreur", récupère la balise qui affichera le message
        document.getElementById('erreur').innerHTML = erreur
-        return false
+        return false 
     }else {
-       
-        alert('formulaire envoyé')
+        alert('formulaire envoyé') //Si tous les champs sont remplis, une alerte s'affichera
+
+        let coordonnée = {
+            nom : lastName.value,
+            prenom : firstName.value,
+            adresse : address.value,
+            ville : city.value,
+            email : email.value 
+        }
+        infoCommande.push(coordonnée)
+        infoCommande.push(productLocalStorage)
+        console.log(infoCommande);
+
     }
+
 
 })
--- Frontend/produit.js
+++ Frontend/produit.js
@@ -15,25 +15,43 @@ let response = fetch (`http://localhost:3000/api/teddies/${leId}`)
 .then(res => {
     if(res.ok){
         res.json().then(data => {
+
+          class recoveredProducts{
+            constructor(name, price, imageUrl, description, colors){
+              this.name = name
+              this.price = price
+              this.imageUrl = imageUrl
+              this.description =  description
+              this.colors = colors
+            }
+          }
+
+          let recoveredProduct = new recoveredProducts (data.name, data.price, data.imageUrl, data.description, data.colors)   
+
             const produit = document.createElement('div') //création d'une div qui contiendra notre code html
             produit.innerHTML =  //insération de notre code dans la div 
         `
             <div class="card">
-            <img src="${data.imageUrl}" alt="">
+            <img src="${recoveredProduct.imageUrl}" alt="">
             <div class="body-card">
               <div class="row">
-                <h4 class="title-card">${data.name}</h4></div>
-                <p class="price-card" id="test">${data.price / 100} €</p>
+                <h4 class="title-card">${recoveredProduct.name}</h4></div>
+                <p class="price-card" id="test">${recoveredProduct.price / 100} €</p>
 
                   <form>
                     <label for="colors">Couleurs</label>
                     <select name="couleur" id="colors"></select> </br>
 
                     <label for="quantity">Quantité</label>
-                    <input type="number" id="quantity" min="1" value="1"> 
+                    <select id="quantity" name="q">
+                      <option value="1" selected>1</option>
+                      <option value="2">2</option>
+                      <option value="3">3</option>
+                      <option value="4">4</option>
+                      <option value="5">5</option>
+                    </select>
                   </form>
-
-                <p class="card-description">${data.description}</p>
+                <p class="card-description">${recoveredProduct.description}</p>
             </div>
           </div>
         `
@@ -41,11 +59,11 @@ let response = fetch (`http://localhost:3000/api/teddies/${leId}`)
 
         // Boucle qui nous permet de récupérer les différentes couleurs pour notre select
 
-        for(let i = 0; i < data.colors.length; i++){
+        for(let i = 0; i < recoveredProduct.colors.length; i++){
           let sel = document.getElementById('colors');
           let opt = document.createElement('option');
           opt.value = i;
-          opt.text = data.colors[i];
+          opt.text = recoveredProduct.colors[i];
           sel.add(opt,null);
         }
 
@@ -56,15 +74,15 @@ let response = fetch (`http://localhost:3000/api/teddies/${leId}`)
 
         // Ajout d'un évènement au clic sur le bouton
         btnEnvoyer.addEventListener('click', (e) =>{
-          e.preventDefault() // annule le comportement par défault (rechargement de la page)
+         e.preventDefault() // annule le comportement par défault (rechargement de la page)
           const quantity = document.getElementById('quantity').value    //récupération de la valeur de l'id quantity
           const colors = document.getElementById("colors").options[document.getElementById('colors').selectedIndex].text;   // récupération du text dans les champs options
           
           let infoProduct = {  // création d'un objet qui contiendra les informations du produit séléctionner par l'utilisateur
             color : colors,
             quantity : quantity,
-            name : data.name,
-            price : data.price / 100,
+            name : recoveredProduct.name,
+            price : recoveredProduct.price / 100,
             id : leId
           }
 
@@ -76,14 +94,10 @@ let response = fetch (`http://localhost:3000/api/teddies/${leId}`)
             localStorage.setItem("produit", JSON.stringify(productLocalStorage)) // Transformation en format JSON et envois dans la key "produit" du local storage
           }
           // si il y a déjà des produits dans le local storage
-          if(productLocalStorage){
-            ajoutProduitLocalStorage()
-          // si il n'y a pas de produit dans le local storage
-          }else{
+          if(productLocalStorage == null){
             productLocalStorage=[];
-            ajoutProduitLocalStorage()
           }
-          
+            ajoutProduitLocalStorage()
           })
         
         // ------------------------------ Fin Ajout au panier --------------------------------------------//
